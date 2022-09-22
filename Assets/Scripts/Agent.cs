84ac371b74c03f4e1cfb0a7d9b2910f4cbc1fd49
RobotGandhi <99viktor.vc@gmail.com>
2022-09-22 14:50:46 +0200

    Updated Agent

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -7,9 +7,10 @@ using UnityEngine.AI;
 
 public class Agent : MonoBehaviour
 {   
-    public Transform goal;
-    public Animator animator;
+    public Vector3 goal;
+    private Animator animator;
     private NavMeshAgent agent;
+    private readonly float[] speeds = {0.0f, 3.5f};
     
     void Start () {
         animator = GetComponent<Animator>();
@@ -17,16 +18,22 @@ public class Agent : MonoBehaviour
         StartCoroutine(updateAnimState());
     }
 
-    public void GiveGoal(Transform trans){
-        goal = trans;
-        agent.destination = trans.position;
+    public void GiveGoal(Vector3 newGoal, int urgency){
+        goal = newGoal;
+        agent.destination = newGoal;
+        if(urgency >= 0 && urgency < speeds.Length) agent.speed = speeds[urgency];
+        else agent.speed = speeds[0];
     }
 
     IEnumerator updateAnimState(){
         while(true){
-            animator.SetBool("moving", 
-                goal != null ? (new Vector2(goal.position.x - transform.position.x, goal.position.z - transform.position.z).sqrMagnitude) > 0.5f : false);
-            Debug.Log(goal == null);
+            //Default: No target available, should not move.
+            bool shouldMove = false;
+            if(goal != null) {
+                shouldMove = new Vector2(goal.x - transform.position.x, goal.z - transform.position.z).sqrMagnitude > 0.5f;
+            }
+            //TODO: If we have several agent speeds, probably change this to int or float
+            animator.SetBool("moving", shouldMove);
             yield return new WaitForSeconds(.1f);
         }
     }
