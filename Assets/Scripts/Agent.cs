2234c3eaa6779c80a523a2dbd2f9b5fe30b1be6c
RobotGandhi <99viktor.vc@gmail.com>
2022-10-09 17:50:03 +0200

    No teleporting (ish)

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -50,8 +50,9 @@ public class Agent : MonoBehaviour
         {
             foreach (NavMeshObstacle obstacle in obstacles)
             {
-                if (obstacle.enabled)
+                if (obstacle.enabled && obstacle.shape == NavMeshObstacleShape.Box)
                 {
+                    float moveSpeed = 30 * Time.deltaTime;
                     RaycastHit hit;
                     Vector3 lookDir = transform.forward;
                     Vector3 betterLookDir = new Vector3(lookDir.x, 0, lookDir.z);
@@ -59,14 +60,18 @@ public class Agent : MonoBehaviour
                     float maxDistance = (fov.viewRadius * fov.susPercentage / 100) * 1.2f;
                     if (Physics.Raycast(transform.position, betterLookDir, out hit, maxDistance, fov.obstacleMask))
                     {
-                        obstacle.center = new Vector3(0, 0, hit.distance / 2);
-                        obstacle.size = new Vector3(hit.distance / 2.5f, 0.5f, hit.distance);
+                        obstacle.center = Vector3.MoveTowards(obstacle.center, new Vector3(0, 0, hit.distance / 2), moveSpeed);
+                        //obstacle.center = new Vector3(0, 0, hit.distance / 2);
+                        obstacle.size = Vector3.MoveTowards(obstacle.size, new Vector3(hit.distance / 2.5f, 0.5f, hit.distance), moveSpeed);
+                        //new Vector3(hit.distance / 2.5f, 0.5f, hit.distance);
                         Debug.DrawRay(transform.position, hit.point - transform.position, Color.red, 0.1f);
                     }
                     else
                     {
-                        obstacle.center = new Vector3(0, 0, maxDistance / 2);
-                        obstacle.size = new Vector3(maxDistance / 2.5f, 0.5f, maxDistance);
+                        obstacle.center = Vector3.MoveTowards(obstacle.center, new Vector3(0, 0, maxDistance / 2), moveSpeed);
+                        //obstacle.center = new Vector3(0, 0, maxDistance / 2);
+                        obstacle.size = Vector3.MoveTowards(obstacle.size, new Vector3(maxDistance / 2.5f, 0.5f, maxDistance), moveSpeed);
+                        //obstacle.size = new Vector3(maxDistance / 2.5f, 0.5f, maxDistance);
                     }
                 }
             }
